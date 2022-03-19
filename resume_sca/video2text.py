6f72621a325bb533469e54a274b8029c1576eace
oraby8 <oraby888@gmail.com>
2022-03-19 17:25:45 +0200

    a

--- resume_sca/video2text.py
+++ resume_sca/video2text.py
@@ -21,15 +21,15 @@ def get_large_audio_transcription(path):
     sound = AudioSegment.from_wav(path)
 
     # split audio sound where silence is 700 miliseconds or more and get chunks
-    chunks = split_on_silence(sound,
+    '''chunks = split_on_silence(sound,
         # experiment with this value for your target audio file
-        min_silence_len = 500,
+        min_silence_len = 1000,
         # adjust this per requirement
         silence_thresh = sound.dBFS-14,
-        # keep the silence for 1 second, adjustable asss
-        keep_silence=500,
-    )
-    #chunks=make_chunks(sound,800)
+        # keep the silence for 1 second, adjustable as well
+        keep_silence=100,
+    )'''
+    chunks=make_chunks(sound,5000)
     folder_name = "audio-chunks"
     # create a directory to store the audio chunks
     if not os.path.isdir(folder_name):
